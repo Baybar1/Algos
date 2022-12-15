a1d3b5f4290c4c9929c987c1392fa3d71cea9c5d
Branley J Aybar <branleyjraybar@gmail.com>
2022-12-15 14:42:25 -0500

    SLL

---
+++ Fronts/script.js
@@ -0,0 +1,125 @@
+//Add front
+
+// class Node {
+//     constructor(data) {
+//         this.data = data;
+//         this.next = null;
+//     }
+// }
+
+// class LinkedList {
+//     constructor() {
+//         this.head = null;
+//     }
+//     addFront(val) {
+//         let newNode = new Node(val);
+//         if (!this.head) {
+//             this.head = newNode;
+//             return this;
+//         } 
+//         newNode.next = this.head;
+//         this.head = newNode;
+//         return this;
+//     } 
+// }
+
+// let LinkedList1 = new LinkedList()
+// LinkedList1.addFront(18)
+// LinkedList1.addFront(5)
+// LinkedList1.addFront(73)
+// console.log(LinkedList1.head)
+
+//Remove Front
+
+// class Node {
+//     constructor(data) {
+//         this.data = data;
+//         this.next = null;
+//     }
+// }
+
+// class LinkedList {
+//     constructor() {
+//         this.head = null;
+//     }
+//     addFront(val) {
+//         let newNode = new Node(val);
+//         if (!this.head) {
+//             this.head = newNode;
+//             return this;
+//         } 
+//         newNode.next = this.head;
+//         this.head = newNode;
+//         return this;
+//     } 
+//     removeFront() {
+//         if (!this.head) {
+//             return;
+//         }
+//         this.head = this.head.next;
+//     }
+    
+// }
+
+// let LinkedList1 = new LinkedList()
+// LinkedList1.addFront(18)
+// LinkedList1.addFront(5)
+// LinkedList1.addFront(73)
+// console.log(LinkedList1.head)
+// LinkedList1.removeFront()
+// console.log(LinkedList1.head)
+// LinkedList1.removeFront()
+// console.log(LinkedList1.head)
+// LinkedList1.removeFront()
+// console.log(LinkedList1.head)
+
+// Front
+
+// class Node {
+//     constructor(data) {
+//         this.data = data;
+//         this.next = null;
+//     }
+// }
+
+// class LinkedList {
+//     constructor() {
+//         this.head = null;
+//     }
+//     addFront(val) {
+//         let newNode = new Node(val);
+//         if (!this.head) {
+//             this.head = newNode;
+//             return this;
+//         } 
+//         newNode.next = this.head;
+//         this.head = newNode;
+//         return this;
+//     } 
+//     removeFront() {
+//         if (!this.head) {
+//             return;
+//         }
+//         this.head = this.head.next;
+//     }
+//     front() {
+//         return this.head.data;
+//     }
+// }
+
+// let LinkedList1 = new LinkedList()
+// LinkedList1.addFront(18)
+// LinkedList1.addFront(5)
+// LinkedList1.addFront(73)
+// console.log(LinkedList1.head)
+// console.log(LinkedList1.front()
+// )
+// LinkedList1.removeFront()
+// console.log(LinkedList1.front()
+// )
+// LinkedList1.removeFront()
+// console.log(LinkedList1.front()
+// )
+// LinkedList1.removeFront()
+// console.log(LinkedList1.front()
+// )
